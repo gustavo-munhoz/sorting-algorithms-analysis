4518aca84ad5ddcd29bc9db10f5feeb4fde43384
rafaelvenetikides <rafavenetikides@gmail.com>
2025-04-27 20:29:05 -0300

    FIX: corrected bubble and insertion sort in python

--- python/src/algorithms/bubble.py
+++ python/src/algorithms/bubble.py
@@ -6,8 +6,8 @@ class BubbleSort(SortAlgorithm):
 
     def sort(self, data, m):
         for i in range(1, len(data)):
-            for j in range(0, len(data) - i):
+            for j in range(0, len(data) - i - 1):
                 m.comparisons += 1
-                if data[j] > data[j + i]:
-                    data[j], data[j + i] = data[j + i], data[j]
+                if data[j] > data[j + 1]:
+                    data[j], data[j + 1] = data[j + 1], data[j]
                     m.swaps += 1
--- python/src/algorithms/insertion.py
+++ python/src/algorithms/insertion.py
@@ -7,13 +7,12 @@ class InsertionSort(SortAlgorithm):
     def sort(self, data, m):
         for i in range(1, len(data)):
             key = data[i]
-            j = i
-            while j >= 0:
-                m.comparisons += 1
-                if key < data[j - 1]:
-                    break
+            j = i - 1
+            m.comparisons += 1
 
-                data[j] = data[j - 1]
+            while j >= 0 and data[j] > key:
+                data[j + 1] = data[j]
                 j -= 1
                 m.swaps += 1
-            data[j] = key
+
+            data[j + 1] = key
