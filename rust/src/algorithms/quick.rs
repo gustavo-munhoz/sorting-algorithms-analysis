f99458aff91a27a64eb697e074dcbc032b9c8916
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-04-27 00:07:49 -0300

    Implemented quick sort

--- rust/src/algorithms/quick.rs
+++ rust/src/algorithms/quick.rs
@@ -1,3 +1,4 @@
+use std::mem::swap;
 use super::base::SortAlgorithm;
 
 pub struct QuickSort;
@@ -8,6 +9,32 @@ impl SortAlgorithm for QuickSort {
     }
 
     fn sort(&self, data: &mut [i32]) {
-        todo!()
+        self.quick_sort(data, 0, data.len() - 1);
+    }
+}
+
+impl QuickSort {
+    fn quick_sort(&self, data: &mut [i32], low: usize, high: usize) {
+        if low < high {
+            let pivot_location = self.partition(data, low, high);
+            self.quick_sort(data, low, pivot_location);
+            self.quick_sort(data, pivot_location + 1, high);
+        }
+    }
+
+    fn partition(&self, data: &mut [i32], low: usize, high: usize) -> usize {
+        let pivot = data[low];
+        let mut left_wall = low;
+
+        for i in (low + 1)..=high {
+            if data[i] < pivot {
+                left_wall += 1;
+                data.swap(i, left_wall);
+            }
+        }
+
+        data.swap(low, left_wall);
+
+        left_wall
     }
 }
