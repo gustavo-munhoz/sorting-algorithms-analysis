060d8c4822dc5d02434d1e72b77f0e23ec9bea09
rafaelvenetikides <rafavenetikides@gmail.com>
2025-04-27 20:31:44 -0300

    FIX: fixed bubble sort not starting from zero in python

--- python/src/algorithms/bubble.py
+++ python/src/algorithms/bubble.py
@@ -5,7 +5,7 @@ class BubbleSort(SortAlgorithm):
     name = "BubbleSort"
 
     def sort(self, data, m):
-        for i in range(1, len(data)):
+        for i in range(len(data)):
             for j in range(0, len(data) - i - 1):
                 m.comparisons += 1
                 if data[j] > data[j + 1]:
