9640bf825a3fad999d2c70bfe5105b7274a94b7b
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-04-26 23:29:30 -0300

    Implemented bubble sort

--- rust/src/algorithms/bubble.rs
+++ rust/src/algorithms/bubble.rs
@@ -8,6 +8,12 @@ impl SortAlgorithm for BubbleSort {
     }
 
     fn sort(&self, data: &mut [i32]) {
-        todo!()
+        for i in 1..data.len() {
+            for j in 0..data.len() - i {
+                if data[j] > data[j + 1] {
+                    data.swap(j, j + 1);
+                }
+            }
+        }
     }
 }
