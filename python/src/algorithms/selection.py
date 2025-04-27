fabb1619c30be2cc6f271dab48ae3dd55f443f7d
André Wozniack <84557148+AndreWozniack@users.noreply.github.com>
2025-04-27 12:30:28 -0300

    implementation of the Selection Sort

--- python/src/algorithms/selection.py
+++ python/src/algorithms/selection.py
@@ -5,5 +5,10 @@ class SelectionSort(SortAlgorithm):
     name = "SelectionSort"
 
     def sort(self, data):
-        # TODO: Import selection sort
+        for i in range(len(data)):
+            min_index = i
+            for j in range(i + 1, len(data)):
+                if data[j] < data[min_index]:
+                    min_index = j
+            data[i], data[min_index] = data[min_index], data[i]
         pass
