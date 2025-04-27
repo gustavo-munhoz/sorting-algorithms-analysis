bca1367cb6e700af962c592c58fde5d60d60c5da
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-04-27 00:07:25 -0300

    Refactored some code of merge sort

--- rust/src/algorithms/merge.rs
+++ rust/src/algorithms/merge.rs
@@ -2,8 +2,29 @@ use super::base::SortAlgorithm;
 
 pub struct MergeSort;
 
+impl SortAlgorithm for MergeSort {
+    fn name(&self) -> &'static str {
+        "MergeSort"
+    }
+
+    fn sort(&self, data: &mut [i32]) {
+        if data.len() <= 1 {
+            return;
+        }
+
+        let middle = data.len() / 2;
+        let (left,right) = data.split_at_mut(middle);
+        self.sort(left);
+        self.sort(right);
+
+        let sorted = self.merge(left, right);
+
+        data.copy_from_slice(&sorted);
+    }
+}
+
 impl MergeSort {
-    fn merge(&self, mut data1: &mut [i32], mut data2: &mut [i32]) -> Vec<i32> {
+    fn merge(&self, data1:  &[i32], data2: &[i32]) -> Vec<i32> {
         let mut sorted = Vec::with_capacity(data1.len() + data2.len());
 
         let mut index1 = 0;
@@ -24,26 +45,5 @@ impl MergeSort {
             sorted.extend_from_slice(&data2[index2..]);
         }
         sorted
-    }
-}
-
-impl SortAlgorithm for MergeSort {
-    fn name(&self) -> &'static str {
-        "MergeSort"
-    }
-
-    fn sort(&self, data: &mut [i32]) {
-        if data.len() <= 1 {
-            return;
-        }
-
-        let middle = data.len() / 2;
-        let (left,right) = data.split_at_mut(middle);
-        self.sort(left);
-        self.sort(right);
-
-        let sorted = self.merge(left, right);
-
-        data.copy_from_slice(&sorted);
     }
 }
