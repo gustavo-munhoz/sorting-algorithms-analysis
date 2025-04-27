6ecfb50900f80440a1818464abc86749775238eb
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-04-27 02:28:54 -0300

    Implemented quick sort

--- python/src/algorithms/quick.py
+++ python/src/algorithms/quick.py
@@ -1,9 +1,28 @@
 from src.algorithms.base import SortAlgorithm
 
 
+def quick_sort(data: [int], low: int, high: int) -> None:
+    if low < high:
+        pivot_location = partition(data, low, high)
+        quick_sort(data, low, pivot_location)
+        quick_sort(data, pivot_location + 1, high)
+
+
+def partition(data: list, low: int, high: int) -> int:
+    pivot = data[low]
+    left_wall = low
+
+    for i in range(low + 1, high + 1):
+        if data[i] < pivot:
+            left_wall += 1
+            data[i], data[left_wall] = data[left_wall], data[i]
+
+    data[low], data[left_wall] = data[left_wall], data[low]
+    return left_wall
+
+
 class QuickSort(SortAlgorithm):
     name = "QuickSort"
 
     def sort(self, data):
-        # TODO: Implement quick sort
-        pass
+        quick_sort(data, 0, len(data) - 1)
