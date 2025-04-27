610afbff1016d37e58605194feee0f3df55bedc0
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2025-04-26 23:28:55 -0300

    Removed nearly_sorted

--- rust/src/dataset.rs
+++ rust/src/dataset.rs
@@ -4,14 +4,3 @@ pub fn random_vec(n: usize) -> Vec<i32> {
     let mut rng = rand::rng();
     (0..n).map(|_| rng.random_range(0..n as i32)).collect()
 }
-
-pub fn nearly_sorted(n: usize, swaps: usize) -> Vec<i32> {
-    let mut v: Vec<i32> = (0..n as i32).collect();
-    let mut rng = rand::rng();
-
-    for pair in sample(&mut rng, n, swaps * 2).into_vec().chunks_exact(2) {
-        v.swap(pair[0], pair[1]);
-    }
-
-    v
-}
